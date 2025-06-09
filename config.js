4e03368e94c92c4f7f473d983f57e0f9dd86eaa1
sggayashan <sggayashanonline@gmail.com>
2025-06-09 21:25:55 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-var settings = {
-  telemetry_level: "basic",
-  test_order: "D_U_P", // Download, Upload, Ping
-  getIp_Url: "https://librespeed.de/garbage.php",
-  download_Url: "https://librespeed.de/garbage.php",
-  upload_Url: "https://librespeed.de/empty.php",
-  ping_Url: "https://librespeed.de/empty.php",
-};
