c8bf1dffb338130c2ef790f4098da43fba545cdf
sggayashan <sggayashanonline@gmail.com>
2025-06-09 21:26:17 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-let s = new Speedtest(); // use real LibreSpeed JS backend
-s.onupdate = data => {
-    if (data.testState === 1) document.getElementById("downloadSpeed").innerText = data.dlStatus + " Mbps";
-    if (data.testState === 3) document.getElementById("uploadSpeed").innerText = data.ulStatus + " Mbps";
-};
-document.getElementById("startBtn").onclick = () => s.start();
